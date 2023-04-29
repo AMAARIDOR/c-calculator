d4c5bf096e41e9a3e827e32380c7273186b2ba91
Amaari <arish2k3@gmail.com>
2023-04-29 13:19:47 +0530

    add functions for different operators

--- calculator.c
+++ calculator.c
@@ -10,3 +10,19 @@ int main() {
 
     return 0;
 }
+
+int add(int a, int b) {
+    return (a + b);
+}
+
+int subtract(int a, int b) {
+    return (a - b);
+}
+
+int multiply(int a, int b) {
+    return (a * b);
+}
+
+int divide(int a, int b) {
+    return (a / b);
+}
