6cf418c7d78548c9706ffb5f34a46051152cc859
Amaari <arish2k3@gmail.com>
2023-04-29 13:31:10 +0530

    try out res variable

--- calculator.c
+++ calculator.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 
 int main() {
-    int a, b; 
+    int a, b, res; 
     char operator;
 
     printf("Enter expression: ");
+
     scanf("%d %c %d", &a, &operator, &b);
-    printf("%d %c %d", a, operator, b);
+
+    printf("%d %c %d = %d\n", a, operator, b, res);
 
     return 0;
 }
