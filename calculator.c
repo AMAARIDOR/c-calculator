827f2dc579953420506af1bae4910d7d0c8c0394
Amaari <arish2k3@gmail.com>
2023-04-29 13:51:07 +0530

    final

--- calculator.c
+++ calculator.c
@@ -1,18 +1,5 @@
 #include <stdio.h>
 
-int main() {
-    int a, b, res; 
-    char operator;
-
-    printf("Enter expression: ");
-
-    scanf("%d %c %d", &a, &operator, &b);
-
-    printf("%d %c %d = %d\n", a, operator, b, res);
-
-    return 0;
-}
-
 int add(int a, int b) {
     return (a + b);
 }
@@ -27,4 +14,27 @@ int multiply(int a, int b) {
 
 int divide(int a, int b) {
     return (a / b);
+}
+
+int main() {
+    int a, b, res; 
+    char operator;
+
+    printf("Enter expression: ");
+
+    scanf("%d %c %d", &a, &operator, &b);
+
+    if (operator == '+') {
+        res = add(a, b);
+    } else if (operator == '-') {
+        res = subtract(a, b);
+    } else if (operator == '*') {
+        res = multiply(a, b);
+    } else if (operator == '/') {
+        res = divide(a, b);
+    }
+
+    printf("%d %c %d = %d\n", a, operator, b, res);
+
+    return 0;
 }
